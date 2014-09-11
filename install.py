c215933c75775657a4743476c7db5bfcc2efd60b
hschoi <hschoi@head.cm.cluster>
2014-09-11 14:27:20 +0900

    Moving to github

--- install.py
+++ install.py
@@ -1,19 +1,17 @@
 import os
 HOME = os.getenv("HOME")
 
-import os
 vivaldi_path = os.path.dirname(os.path.abspath(__file__))
 
-print vivaldi_path
-
-f = open(HOME+'/.bash_profile','r')
+f = open(HOME+'/.bashrc','r')
 a = f.read()
 
 b = "\nvivaldi_path=%s\npath=%s/src/py-src\nexport vivaldi_path\nexport PATH=$path:$PATH\n"%(vivaldi_path,vivaldi_path)
 c = a + b
 f.close()
 
-f = open(HOME+'/.bash_profile','w')
+f = open(HOME+'/.bashrc','w')
 f.write(c)
 f.close()
 
+os.system('source ~/.bashrc')
